85cb7536409bd609fe52c79213a2f163989e166d
Brian Brown <briangbrown@gmail.com>
2013-11-17 10:07:17 -0700

    Adding a test that allows a real captured image to be used. This allows testing real world scenarios. Also moved around some things in the test so the image assets could be accessed from the test.

--- tests/src/com/droidtools/rubiksolver/test/ColorDecoderTest.java
+++ tests/src/com/droidtools/rubiksolver/test/ColorDecoderTest.java
@@ -4,26 +4,41 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.test.AndroidTestCase;
+import android.test.InstrumentationTestCase;
+import android.util.Log;
 
 import com.droidtools.rubiksolver.ColorDecoder;
 import com.droidtools.rubiksolver.HColor;
 
-public class ColorDecoderTest extends AndroidTestCase {
-
+public class ColorDecoderTest extends InstrumentationTestCase {
+	
+	private final String TAG = ColorDecoderTest.class.getName();
+    private Context testContext;
+    private Context targetContext;
+	
 	public ColorDecoderTest() {
 		super();
 	}
 	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		this.testContext = getInstrumentation().getContext();
+		this.targetContext = getInstrumentation().getTargetContext();
+	}
+
 	/**
 	 * Tests that the HColor Average method works correctly.
 	 * TODO: move this into HColor.
@@ -39,7 +54,6 @@ public class ColorDecoderTest extends AndroidTestCase {
 		assertEquals(85, avgColor.g);
 		assertEquals(85, avgColor.b);
 	}
-	
 	
 	public void testSyntheticRowThreeColorDecode() {
 		byte[][] colorIndex = new byte[][] {
@@ -67,6 +81,58 @@ public class ColorDecoderTest extends AndroidTestCase {
 		};
 		assertSyntheticCubeDecode(colorIndex);
 	}
+	
+	// TODO: Currently failing because we incorrectly decode the cubelet face with the Rubik's
+	// cube logo.
+	public void testImageBasedSixColorWithLogoDecode() {
+		byte[][] colorIndex = new byte[][] {
+				{1, 4, 2}, // [0][0], [0][1], [0][2]
+				{1, 3, 3}, // [1][0], [1][1], [1][2]
+				{5, 1, 0}  // [2][0], [2][1], [2][2]
+		};
+		assertImageBasedCubeDecode(colorIndex, "cube_face_600x450_6colors.png");
+	}
+	
+	/**
+	 * Tests an image captured cube with the decode method making sure the right number of
+	 * colors and the correct colors are decoded.
+	 */
+	private void assertImageBasedCubeDecode(byte[][] colorIndex, String fileName) {
+		Set<Byte> uniqueInputIndex = new TreeSet<Byte>();
+		for (int row = 0; row < 3; row++) {
+			for (int col = 0; col < 3; col++) {
+			    uniqueInputIndex.add(colorIndex[row][col]);
+			}
+		}
+		
+		ColorDecoder decoder = new ColorDecoder(targetContext.getCacheDir().getAbsolutePath(),
+				targetContext.getExternalFilesDir(null).getAbsolutePath());
+		byte[] ids = decoder.decode(readTestImage(fileName));
+		
+		// There should always be 9 cubelets.
+		assertEquals(9, ids.length);
+		
+		Set<Byte> uniqueIds = new TreeSet<Byte>();
+		for (int i = 0; i < ids.length; i++) {
+			uniqueIds.add(ids[i]);
+		}
+		
+		// Number of different colors decoded.
+		assertEquals(uniqueInputIndex.size(), uniqueIds.size());
+		
+		// Verify each color
+		for (int row = 0; row < 3; row++) {
+			for (int col = 0; col < 3; col++) {
+				// Note that ids is in Col-Major order starting in the bottom left.
+				// 2 5 8
+				// 1 4 7
+				// 0 3 6
+				HColor decodedColor = decoder.getColor(ids[(col + 1) * 3 - (row + 1)]);
+				HColor expectedColor = new HColor(getColorFromIndex(colorIndex[row][col]));
+				assertEquals(0, expectedColor.distance(decodedColor), 0.0000001);
+			}	
+		}
+	}
 
 	/**
 	 * Tests a synthetically created cube with the decode method making sure the right number of
@@ -80,8 +146,8 @@ public class ColorDecoderTest extends AndroidTestCase {
 			}
 		}
 		
-		ColorDecoder decoder = new ColorDecoder(getContext().getCacheDir().getAbsolutePath(),
-				getContext().getExternalFilesDir(null).getAbsolutePath());
+		ColorDecoder decoder = new ColorDecoder(targetContext.getCacheDir().getAbsolutePath(),
+				targetContext.getExternalFilesDir(null).getAbsolutePath());
 		byte[] ids = decoder.decode(getTestCubeBitmap(colorIndex));
 		
 		// There should always be 9 cubelets.
@@ -207,14 +273,29 @@ public class ColorDecoderTest extends AndroidTestCase {
 	 */
 	private void saveToExternalFile(Bitmap image, String fileName) {
 	    try {
-	        File file = new File(getContext().getExternalFilesDir(null), fileName);
+	        File file = new File(testContext.getExternalFilesDir(null), fileName);
 	        FileOutputStream fos = new FileOutputStream(file);
 	        image.compress(Bitmap.CompressFormat.PNG, 90, fos);
 	        fos.close();
 	    } catch (FileNotFoundException e) {
-	        e.printStackTrace();
+	    	Log.e(TAG, String.format("Failed to save %s to external media", fileName), e);
 	    } catch (IOException e) {
-	        e.printStackTrace();
+	    	Log.e(TAG, String.format("Failed to save %s to external media", fileName), e);
 	    }
+	}
+	
+	/**
+	 * Read a test image from the assets folder in the test.
+	 * @param fileName of the test image
+	 * @return a bitmap of the loaded image
+	 */
+	private Bitmap readTestImage(String fileName) {
+		try {
+			InputStream is = testContext.getAssets().open("images/" + fileName);
+			return BitmapFactory.decodeStream(is);
+		} catch (IOException e) {
+			Log.e(TAG, String.format("Failed to open %s from assets", fileName), e);
+		}
+		return null;
 	}
 }
